903286cea827093d941344dc6e7648a0443e0324
WatsonSantos <watsontavares72@gmail.com>
2022-07-13 19:21:02 +0100

    versão final para apresentação

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 
-
 const form = document.getElementById("formulario")
-
 const cepInput = document.getElementById("input-dados")
-
-
 
 class Address{
     constructor({logradouro, bairro, localidade}){
@@ -18,23 +14,29 @@ class Address{
 class Api{
     static async getAddress(cep){
       const response = new Address((await axios.get(`https://viacep.com.br/ws/${cep}/json/`)).data)
-       
-      dado1.textContent = response.logradouro
-      dado2.innerHTML = response.bairro
-      dado3.innerHTML = response.localidade
-      console.log(response)
+     if(!response || response.logradouro == undefined){
+        alert("Informe um cep válido!")
+     }else{
 
+        dado1.textContent = response.logradouro
+       dado2.innerHTML = response.bairro
+       dado3.innerHTML = response.localidade
+       console.log(response)
+
+     }
+      
+      
 }
 }
 
-
+//69082-542
 form.addEventListener('submit', (e)=>{
-
-   
-      Api.getAddress(cepInput.value).then(()=>{
+    
+    Api.getAddress(cepInput.value).then(()=>{
         console.log("Dados encontrados")}
         ).catch((err)=>{
             alert("Houve um erro, tente um cep válido ou tente mais tarde.")  
+            console.log(err)
         })
 
     e.preventDefault()
